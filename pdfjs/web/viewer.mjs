fc713b0ccaf31c75ea513c03394421f6a740809f
thorsproject <project.thor1965@freenet.de>
2026-02-24 15:38:23 +0100

    test

--- pdfjs/web/viewer.mjs
+++ pdfjs/web/viewer.mjs
@@ -783,7 +783,7 @@ const defaultOptions = {
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   enableScripting: {
-    value: false,
+    value: true,
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   enableSignatureEditor: {
